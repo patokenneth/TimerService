b02db5b84d675294466b7ff6990c704d94a074c7
Patokenneth <chiemezieoloto4@gmail.com>
2021-05-26 04:26:55 +0100

    Full working app

---
+++ Timer/Service1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timer
+{
+    public partial class Service1 : ServiceBase
+    {
+        System.Timers.Timer timer = new System.Timers.Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("service started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object s, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recalled at " + DateTime.Now);
+        }
+
+        public void WriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\logs\\Service";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = new StreamWriter(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(filepath, true))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
